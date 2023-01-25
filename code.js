3e995426df835be09771e49e6af2e358e7635e3d
Parker Stomm <parker.g.stomm@gmail.com>
2023-01-25 17:46:53 -0500

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -3,6 +3,17 @@ var  helloWorld = function(){
     return "Hello, World!";
 }
 
-function sayHello(){
-    return "";
+// function sayHello(input){
+//     if(input === "Alex"){
+//         return "Hello, Alex!";
+//     } else if(input === "Jane") {
+//         return "Hello, Jane!";
+//     } else {
+//         return "Hello, Pat!";
+//     }
+// }
+
+// RE-Factor of sayHello Function
+function sayHello(input){
+    return "Hello, " + input + "!";
 }
